18111f5d17ee5bf566dbbce6d18e65bbbaaa2b69
Steve Gordon <steve.gordon@madgex.com>
2018-10-08 08:59:45 +0100

    Adding console for debugging

---
+++ src/ConsoleApp/IMyThingA.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp
+{
+    interface IMyThingA
+    {
+        string SayHello();
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddScoped<IMyThingA, MyThingA>();
+
+            serviceCollection.AddScoped<MyThingB>();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            for (var i = 0; i < 10; i++)
+            {
+                serviceProvider.GetService<IMyThingA>();
+            }
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+
+    class MyThingA : IMyThingA
+    {
+        private readonly MyThingB _thingB;
+
+        public MyThingA(MyThingB thingB)
+        {
+            _thingB = thingB;
+        }
+
+        public string SayHello()
+        {
+            return _thingB.Hello();
+        }
+    }
+
+    class MyThingB
+    {
+        public string Hello()
+        {
+            return "Hi";
+        }
+    }
+}
